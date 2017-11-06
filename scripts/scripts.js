93e9fe64cc239c783bb4031191e44e2e5e940b81
elliot4959 <elliot4959@live.co.uk>
2017-11-06 17:15:44 +0000

    Added local storage functionality for JavaScript page, also set this as default HTML page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,73 @@
-(function(){
+(function () {
   let sentence = document.getElementById("sentence");
   let sentForm = document.getElementById("sentence-form");
   let blankSpace = document.getElementById("space-for-paragraphs");
-	
+  let counter = 0;
 
-  function doStuff(ev) {
-    let paragraph = document.createElement("li");
-    let txt = document.createTextNode(sentence.value);
-    paragraph.appendChild(txt);
-    blankSpace.appendChild(paragraph);
-    ev.preventDefault();
+  // Get all the data stored in local storage
+  if (localStorage.length > 0) {
+    // For each item stored in local storage
+    for (let i=0; i<localStorage.length; i++) {
+      // Write that item of data
+      writeSaved(localStorage.getItem(i));
+      // Increment the counter so that new items are appended to the list
+      counter++;
+    }
   }
 
-  sentForm.addEventListener("submit", doStuff);
-	
+  function writeValue(page, data) {
+    // If the entry is not empty
+    if (sentence.value != "") {
+      // Store the sentence value within the scope of this function
+      let sentenceValue = sentence.value;
+      // Create a new list item
+      let paragraph = document.createElement("li");
+      // variable to store text from user
+      let txt = document.createTextNode(sentenceValue);
+      // Empty the text box
+      sentence.value = "";
+      // Add the user text to the html list item
+      paragraph.appendChild(txt);
+      // Write the user text to the web page
+      blankSpace.appendChild(paragraph);
+      // Save to local storage
+      localStorage.setItem(counter.toString(), sentenceValue);
+      // Increment the counter
+      counter++;
+    } else {
+      alert("You cannot enter an empty value");
+    }
+    // Prevent page reload
+    page.preventDefault();
+  }
+
+  function writeSaved(data) {
+    // Create a new list item
+    let paragraph = document.createElement("li");
+    // variable to store text from user
+    let txt = document.createTextNode(data);
+    // Add the user text to the html list item
+    paragraph.appendChild(txt);
+    // Write the user text to the web page
+    blankSpace.appendChild(paragraph);
+  }
+
+  function clearData() {
+    // Check there is data to clear
+    if (localStorage.length > 0){
+      // Clear the local data
+      localStorage.clear();
+
+      // Reset the HTML
+      blankSpace.innerHTML = "";
+    }
+    // If no data stored, alert the user.
+    else {
+      alert("No data stored!");
+    }
+  }
+
+  // Add a listener for the submit action, at which point the writeValue method is run
+  sentForm.addEventListener("submit", writeValue);
+  sentForm.addEventListener("reset", clearData)
 }());
